d54e94088a4bf6bed5870c15e42b1bf3fd14e80d
M K <mkpro118@gmail.com>
2025-08-30 12:15:11 -0700

    Initialize Rust project for prisma schema to rust types codegen
    Added initial files for a Rust project, including Cargo configuration and strict
    lint rules.

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#![deny(clippy::expect_used)] // using deny so that test code can use it
+#![deny(clippy::style)]
+#![deny(clippy::unwrap_used)] // using deny so that test code can use it
+#![deny(unsafe_code)]
+#![forbid(clippy::allow_attributes)]
+#![forbid(clippy::complexity)]
+#![forbid(clippy::correctness)]
+#![forbid(clippy::pedantic)]
+#![forbid(clippy::perf)]
+#![forbid(clippy::suspicious)]
+#![forbid(future_incompatible)]
